9a3748cfc45d24f2b76e874c2f4bc52b77168d54
adel <adelbouhafs621@gmail.com>
2024-11-26 10:57:54 +0100

    commit #3 programme test

--- TP5_AdelBOUHAFS/Transgerbeurcpp.cpp
+++ TP5_AdelBOUHAFS/Transgerbeurcpp.cpp
@@ -7,36 +7,63 @@
 
 using namespace std;
 
+enum EEtat
+{
+	Vide, Palette, Rouleau
+
+};
+
+struct SPalette
+{
+	int poids;
+	char reference[19];
+
+};
+
+struct SRouleau
+{
+	float longeur;
+	int numero;
+};
+
+union UContenu
+{
+	SPalette paletteCasier;
+	SRouleau rouleauCasier;
+};
+
+struct SCasier
+{
+	int px = 0;
+	int py = 0;
+	UContenu contenu;
+	EEtat etat;
+};
+
+
 int main()
 {
 
 	int tgb[9][2];
-	int px = 0;
-	int py = 0;
-	union UContenu
-	{
-		struct SPalette;
-		struct SRouleau;
-	};
+	SRouleau ro1;
+	SPalette pa1;
+	SCasier c1;
+	UContenu cont;
 
-	struct SPalette
-	{
-		int poids;
-		char* reference[19];
+	pa1.poids = 15;
+	strcpy_s(pa1.reference, "palette1");
+	ro1.longeur = 10;
+	ro1.numero = 5;
+	cont.paletteCasier = pa1;
+	cont.rouleauCasier = ro1;
+	
+	cont.paletteCasier = pa1;
+	c1.contenu.paletteCasier = pa1;
+	c1.etat = Palette;
+	c1.px = 2;
+	c1.py = 3;
 
-	};
 
-	struct SRouleau
-	{
-		float longeur;
-		int numero;
-	};
-
-	enum EEtat
-	{
-		vide, SPalette, SRouleau
-		
-	};
 	cout << "Programme du transgerbeur" << "\n";
 
 	
