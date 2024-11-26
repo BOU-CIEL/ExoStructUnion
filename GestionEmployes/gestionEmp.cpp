56079486bc7a1bebdfed47a3df0ba7a2aeb5e483
adel <adelbouhafs621@gmail.com>
2024-11-26 09:13:19 +0100

    commit #2 déclaration structures

---
+++ GestionEmployes/gestionEmp.cpp
@@ -0,0 +1,12 @@
+//Nom du TP= TP_5struct enum union
+//Date de création=  26/11/2024
+//Nom de l'élève= Adel BOUHAFS 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Programme de gestion des employes" << "\n";
+
+}
---
+++ TP5_AdelBOUHAFS/Transgerbeurcpp.cpp
@@ -0,0 +1,44 @@
+//Nom du TP= TP_5struct enum union
+//Date de création=  26/11/2024
+//Nom de l'élève= Adel BOUHAFS 
+
+#include <iostream>
+#include <complex>
+using namespace std;
+
+int main()
+{
+
+	int tgb[9][2];
+	int px = 0;
+	int py = 0;
+	union UContenu
+	{
+		struct SPalette;
+		struct SRouleau;
+	};
+
+	struct SPalette
+	{
+		int poids;
+		char* reference[19];
+
+	};
+
+	struct SRouleau
+	{
+		float longeur;
+		int numero;
+	};
+
+	enum EEtat
+	{
+		vide, SPalette, SRouleau
+		
+	};
+	cout << "Programme du transgerbeur" << "\n";
+
+	
+
+
+}
